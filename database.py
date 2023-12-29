9cca56d92a5ed148e9f9f3761eab65e8c572edd5
Venkat3103 <venkatasubramanian2001@gmail.com>
2023-12-29 14:44:56 +0300

    added order insertion functions

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import config
 import pandas as pd
+import numpy as np
+import streamlit as st
 import snowflake.connector
 
 def get_database_connection():
@@ -32,7 +34,7 @@ def get_initial_df(conn):
 
 def fetch_order_items(conn, order_id):
     items_query = f"""
-        SELECT pc.department, pc.product_name, oi.quantity, oi.unit_price
+        SELECT pc.product_id, pc.department, pc.product_name, oi.quantity, oi.unit_price
         FROM orders.order_items oi JOIN product.product_catalogue pc
         ON oi.product_id = pc.product_id
         WHERE oi.order_id = {order_id}
@@ -77,3 +79,105 @@ def get_consumer_email(conn, order_id):
         on ud.user_id = cd.consumer_id
     """
     return pd.read_sql(query,conn)
+
+def update_order_item(conn, item_df, order_id):
+    print(item_df.dtypes)
+    #item_df['QUANTITY'] = item_df['QUANTITY'].astype(int)
+    print(item_df.dtypes)
+    print("QUERY 1")
+    query1 = f"""
+    UPDATE orders.order_items
+    SET quantity = {item_df['QUANTITY']}, unit_price = {item_df['UNIT_PRICE']}
+    WHERE ORDER_ID = {order_id}
+    AND product_id = {item_df['PRODUCT_ID']}
+    """
+    print("QUERY 2")
+    query2 = f"""
+    UPDATE orders.order_details od
+    SET order_amount = (
+        SELECT COALESCE(SUM(quantity * unit_price), 0) AS new_order_amount
+        FROM orders.order_items
+        WHERE order_id = {order_id}
+    )
+    WHERE od.ORDER_ID = {order_id}
+    """
+
+    try:
+        cursor = conn.cursor()
+        
+        # Begin the transaction
+        cursor.execute("BEGIN TRANSACTION")
+
+        # Execute the first query
+        cursor.execute(query1)
+
+        # Execute the second query
+        cursor.execute(query2)
+
+        # Commit the transaction
+        conn.commit()
+
+        cursor.close()
+        print("Executed")
+        return True  # Return True to indicate success
+
+    except Exception as e:
+        # Handle the exception (e.g., print an error message)
+        print(f"Error executing query: {e}")
+
+        # Rollback the transaction
+        cursor.execute("ROLLBACK")
+
+        return False  # Return False to indicate failure
+
+def insert_new_order_items(conn,df):
+    table_name = 'order_items'
+    batch_size = 1000 
+    cursor = conn.cursor()
+    for i in range(0, len(df), batch_size):
+        batch_df = df.iloc[i:i + batch_size]
+        
+        columns = ', '.join(batch_df.columns)
+        print(columns)
+        placeholders = ', '.join(['%s' for _ in range(len(batch_df.columns))])
+        print(placeholders)
+        insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+
+        # Convert DataFrame to a list of tuples for insertion
+        data_to_insert = [tuple(row) for row in batch_df.values]
+        st.write(data_to_insert)
+        # Execute the SQL query
+        cursor.executemany(insert_query, data_to_insert)
+
+# Commit the transaction
+    conn.commit()
+
+def insert_new_order_details(conn,df):
+    table_name = 'orders.order_items'
+    batch_size = 1000 
+    cursor = conn.cursor()
+    for i in range(0, len(df), batch_size):
+        batch_df = df.iloc[i:i + batch_size]
+        columns = ', '.join(batch_df.columns)
+        print(columns)
+        placeholders = ', '.join(['%s' for _ in range(len(batch_df.columns))])
+        print(placeholders)
+        insert_query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+        st.write(insert_query)
+        # Convert DataFrame to a list of tuples for insertion
+        data_to_insert = [tuple(row) for row in batch_df.values]
+
+        # Execute the SQL query
+        cursor.executemany(insert_query, data_to_insert)
+
+# Commit the transaction
+    conn.commit()
+
+def get_user_ids(username,conn):
+    cid = get_user_details(conn,username)
+    shopper_id_query = """
+                    SELECT USER_ID from user.user_data where USER_CLASS like 'shopper'
+                    """
+    sid = pd.read_sql(shopper_id_query,conn)
+    return cid.USER_ID.loc[0],np.random.choice(sid.USER_ID.to_list())
+
